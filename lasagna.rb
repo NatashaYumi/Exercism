fa19f7d94e943a07342ff6e60610fb3143621b74
NatashaYumi <yumi-nakayama@hotmail.com>
2022-03-18 13:18:53 -0300

    lasagna exercise

---
+++ lasagna.rb
@@ -0,0 +1,18 @@
+class Lasagna
+
+    Lasagna:: EXPECTED_MINUTES_IN_OVEN = 40
+    Lasagna:: PREPARATION_PER_LAYER = 2
+      
+      def remaining_minutes_in_oven(actual_minutes_in_oven)
+        return EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+      end
+    
+      def preparation_time_in_minutes(number_of_layers)
+        return PREPARATION_PER_LAYER * number_of_layers
+      end
+    
+      def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
+        return preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+      end
+    
+    end
